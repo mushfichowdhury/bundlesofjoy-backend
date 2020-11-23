1c584d44e97bbd59e4a2d6db69dad0031cfe3c8b
mushfichowdhury <chowdhurymushfi@gmail.com>
2020-11-23 07:43:42 -0500

    need to fix journal entry seed

--- app/controllers/journal_entries_controller.rb
+++ app/controllers/journal_entries_controller.rb
@@ -36,6 +36,8 @@ class JournalEntriesController < ApplicationController
   # DELETE /journal_entries/1
   def destroy
     @journal_entry.destroy
+
+    render json: @journal_entry
   end
 
   private
@@ -46,6 +48,6 @@ class JournalEntriesController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def journal_entry_params
-      params.require(:journal_entry).permit(:user_id, :time, :image, :content)
+      params.require(:journal_entry).permit(:user_id, :image, :content)
     end
 end
--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module BundlesofjoyBackend
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.eager_load = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
