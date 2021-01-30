99e0fb462e2cf5bbdb26006c53042dceb141b306
mushfichowdhury <chowdhurymushfi@gmail.com>
2021-01-30 11:41:25 -0500

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,6 @@ Rails.application.routes.draw do
       resources :users
       post '/login', to: 'auth#create'
       post '/childlogin', to: 'auth#createchild'
-      get '/profile', to: 'users#profile'
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
